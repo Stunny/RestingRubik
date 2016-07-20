1cb64d18664655c3dbcbca6ea387dcf0447e1465
Alex Vogel <avogelm@gmail.com>
2016-07-21 00:42:32 +0200

    Solving POST

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
 
-mongoose.connect('mongodb://localhost/cube', function(err,res){
-	if(err) console.log('Error al conectar a la base de datos:'+err);
-	else console.log('Conexion a la base de datos realizada');
-});
-
 app.get(function(){
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
 } );
+
+mongoose.connect('mongodb://localhost/cube', function(err,res){
+	if(err) console.log('Error al conectar a la base de datos:'+err);
+	else console.log('Conexion a la base de datos realizada');
+});
+
 
 app.get('/', function(req,res){
 	res.send('Hola Mundo!');
