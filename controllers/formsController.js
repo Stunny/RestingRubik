8bd558247a3ae59bc539b732320856b5ab183d0e
Alex Vogel <avogelm@gmail.com>
2016-07-22 19:11:25 +0200

    Added status codes to visual sites

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -1,17 +1,33 @@
 //---Enviara la pagina de formulari addient a la ruta introduida
 module.exports.indice = function(req, res){
   var path = require('path');
-  res.sendFile(path.join(__dirname,'../view/index.html'));
+  try{
+    res.sendFile(path.join(__dirname,'../view/index.html'));
+  }catch(err){
+    res.status(500);
+  }
 };
 module.exports.cubeForm = function(req, res){
   var path = require('path');
-  res.sendFile(path.join(__dirname,'../view/forms/cubeForm.html'));
+  try{
+    res.sendFile(path.join(__dirname,'../view/forms/cubeForm.html'));
+  }catch(err){
+    res.status(500);
+  }
 };
 module.exports.algForm = function(req, res){
   var path = require('path');
-  res.sendFile(path.join(__dirname,'../view/forms/algForm.html'));
+  try{
+    res.sendFile(path.join(__dirname,'../view/forms/algForm.html'));
+  }catch(err){
+    res.status(500);
+  }
 };
-module.exports.guideForm = function(){
+module.exports.guideForm = function(req, res){
   var path = require('path');
-  res.sendFile(path.join(__dirname,'../view/forms/guideForm.html'));
+  try{
+    res.sendFile(path.join(__dirname,'../view/forms/guideForm.html'));
+  }catch(err){
+    res.status(500);
+  }
 };
